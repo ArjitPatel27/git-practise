a6d5891623a4522d30f78725cc1ad67abffe836e
Arjit Gurjar <patelarjit8@@gmail.com>
2026-06-16 21:50:11 +0530

    added this

--- demo.py
+++ demo.py
@@ -2,12 +2,10 @@ def my_function():
 
     a = 13
     b = 7
-    c = d
+    c = f
 
     return f
-    print("This is a function in demo.py")
-    print(f"The sum of {a} and {b} is {c}")
+   
+  my_function()
 
-    my_function()
-
-    test()
+    
