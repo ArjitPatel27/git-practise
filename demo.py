17c150dba1252ce90486cd8236364f9ad7512e95
Arjit Gurjar <patelarjit8@@gmail.com>
2026-06-16 21:47:33 +0530

    adding tezt

---
+++ demo.py
@@ -0,0 +1,13 @@
+def my_function():
+
+    a = 13
+    b = 7
+    c = d
+
+    return f
+    print("This is a function in demo.py")
+    print(f"The sum of {a} and {b} is {c}")
+
+    my_function()
+
+    test()
